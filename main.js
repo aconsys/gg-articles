be5c4421b9636735cd9fb08923209def5a750559
aconsys <59352174+aconsys@users.noreply.github.com>
2022-08-21 21:11:45 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let dropdownLink = document.querySelectorAll('.dropdown__link');
 
+// let scrollPosition = document.querySelector('.top-section-wrap').clientHeight + parseFloat(getComputedStyle(document.querySelector('.article')).paddingTop);
+
 for (let item of dropdownLink) {
   item.addEventListener('click', () => {
     let listOfClass = item.nextElementSibling.classList;
@@ -8,5 +10,9 @@ for (let item of dropdownLink) {
       item.nextElementSibling.classList.remove('dropdown__content_active');
     };
     listOfClass.add('dropdown__content_active');
+
+    item.scrollIntoView();
+
+    // window.scrollTo(0, scrollPosition);
   });
 };
